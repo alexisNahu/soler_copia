d51b49d0e337d17eeb8189d49f0f6e67241d1ad1
edavg <eduardo.avg95@gmail.com>
2024-04-03 13:29:51 -0400

    vercel

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/profesionales_330GzHc_.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/profesionales_BSHG9oOG.mjs');
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/servicios_Cwi3wc1t.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/servicios_C8M24tWe.mjs');
-
-export { page };
