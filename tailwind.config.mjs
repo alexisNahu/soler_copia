1b746d2e6f72f758e3ebe83d2c94f432cc7fb89a
anig <alexisnahuelidoyaga@gmail.com>
2026-04-20 17:00:01 -0300

    commit inicial

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['"Open Sans"', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
       backgroundImage: {
         hero: "url('/index/solerHero.webp')",
       },
