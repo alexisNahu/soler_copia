6b9c128bdc5f65f316002245092cac221d1b01e4
anig <alexisnahuelidoyaga@gmail.com>
2026-04-20 20:25:42 -0300

    agregado noticias, noticias detalle y seccion de noticias en el index, siguen siendo paginas estaticas

---
+++ src/constans.ts
@@ -0,0 +1,47 @@
+const LOREM_IPSUM = `
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas tristique aenean lorem egestas mattis porta est sit. 
+    Laoreet vulputate malesuada pretium aliquet velit dolor purus. Fermentum tincidunt erat enim malesuada. 
+    Velit id id faucibus rhoncus sit. In adipiscing lobortis sodales phasellus feugiat elementum sem integer.</p>
+    
+    <p>Dictumst non sociis tristique tempus morbi. Omare morbi mauris tristique dictum ante risus purus convallis. 
+    Turpis magna lacus condimentum ultricies massa. Sodales sed est quisque ut consectetur velit. 
+    Massa tempus consequat vel enim enim justo non vitae ullamcorper.</p>
+    
+    <p>Urna lectus elit platea aliquam ac placerat morbi egestas. Semper id nec rhoncus mauris elementum at 
+    pellentesque elementum malesuada. Maecenas enim vitae enim nunc sed. Egestas aliquet pellentesque 
+    ornare ultrices nibh sagittis ipsum. Tristique tempor eu dictum nunc lectus nisl feugiat id.</p>
+    
+    <p>Bibendum pellentesque et quisque massa in nam vestibulum ipsum nisl. Viverra natoque consequat 
+    senectus enim justo. Tristique enim risus convallis sit nec molestie mollis condimentum blandit. 
+    Consectetur hendrerit sed malesuada phasellus et proin eget quis.</p>
+`;
+
+export const noticias = [
+    {
+        slug: 'la-salud-tambien-se-construye',
+        fecha: "14/04/2026",
+        tags: ["ECONOMÍA", "LIDERAZGO"],
+        titulo: "La salud también se construye desde lo que no se ve",
+        img: "/noticias/noticia1.png",
+        href: '/noticias/la-salud-tambien-se-construye',
+        contenido: LOREM_IPSUM
+    },
+    {
+        slug: 'asesoria-integral-regulaciones',
+        fecha: "14/04/2026",
+        tags: ["ECONOMÍA", "LIDERAZGO"],
+        titulo: "Asesoría Integral en Regulaciones y Normativas para empresas del Sector Químico",
+        img: "/noticias/noticia2.png",
+        href: '/noticias/asesoria-integral-regulaciones',
+        contenido: LOREM_IPSUM
+    },
+    {
+        slug: 'paraguay-avanzando-economia-industrial',
+        fecha: "14/04/2026",
+        tags: ["ECONOMÍA", "LIDERAZGO"],
+        titulo: "Paraguay está avanzando hacia una economía cada vez más industrial",
+        img: "/noticias/noticia3.png",
+        href: "/noticias/paraguay-avanzando-economia-industrial",
+        contenido: LOREM_IPSUM
+    }
+];
