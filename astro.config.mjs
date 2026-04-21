369b115be7b02087ad81b9eee5c57c88eab4b2e7
anig <alexisnahuelidoyaga@gmail.com>
2026-04-21 09:18:29 -0300

    configurando vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-import vercelServerless from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
 
   output: "server",
-  adapter: vercelServerless(),
+  adapter: vercel(),
 });
